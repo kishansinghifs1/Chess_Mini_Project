e9a6ab6d664d1bd4078cc8947dcb651d5991bf52
kishansinghifs1 <kishansingh956196@gmail.com>
2024-11-18 10:42:06 +0530

    initial Commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const gameBoard = document.querySelector('#gameBoard');
+const playerDisplay = document.querySelector('#player');
+const infodisplay = document.querySelector('#info-display');
+const width = 8;
+const startPieces = [
+    rook , knight , bishop ,queen, king ,bishop , knight , rook ,
+    pawn , pawn , pawn , pawn , pawn , pawn , pawn , pawn ,
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 
+    ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 
+    pawn , pawn , pawn , pawn , pawn , pawn , pawn , pawn ,
+    rook , knight , bishop ,queen, king ,bishop , knight , rook 
+]
+function createBoard() {
+    startPieces.forEach((startPiece, i) => {
+      const square = document.createElement('div')
+      square.classList.add('square')
+      square.innerHTML = startPiece; 
+      square.setAttribute('square-id', i)
+      square.firstChild?.setAttribute('draggable', 'true')
+      const row = Math.floor((63 -i)/8)+1
+      if(row%2 === 0){
+        square.classList.add(i%2 === 0 ? "beige" : "brown")
+      }else{
+        square.classList.add(i%2 === 0 ? "brown" : "beige")
+      }
+      if( i <= 15){
+         square.firstChild.firstChild.classList.add('black')
+      }
+      if( i >= 48){
+        square.firstChild.firstChild.classList.add('white')
+      }
+      gameBoard.append(square)
+    })
+  }
+createBoard()
+const allSquares = document.querySelectorAll("#gameBoard .square")
+console.log(allSquares)
+// allSquares.forEach(square => {
+//   square.addEventListener('dragstart', dragStart)
+//   square.addEventListener('dragover', dragOver)
+// })
+// let startPostitionId
+// let draggedElement
+// function dragStart (e) {
+//   startPostitionId = e.target.parentNode.getAttribute('square-id')
+//   draggedElement = e.target
+// }
+// function dragOver (e) {
+//   e.preventDefault()
+// }
